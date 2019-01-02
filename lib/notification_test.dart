7216349d5a84c2b2ceba91fe5d55492f4771cbc3
jingyan.wan <wanjingyan001@yahoo.com>
2019-01-02 23:43:55 +0800

    notification widget test

---
+++ lib/notification_test.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class ScrollNotificationTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener(
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text("$index"),
+          );
+        },
+        itemCount: 100,
+      ),
+      onNotification: (notification) {
+        switch (notification) {
+          case ScrollStartNotification:
+            print("开始滚动");
+            break;
+          case ScrollUpdateNotification:
+            print("正在滚动");
+            break;
+          case ScrollEndNotification:
+            print("滚动结束");
+            break;
+          case OverscrollNotification:
+            print("到达边界");
+            break;
+        }
+      },
+    );
+  }
+}
+
+class MyTestNotification extends Notification {
+  MyTestNotification(this.msg);
+
+  String msg;
+  int count = 0;
+}
+
+class MyTestNotificationRoute extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _NotificationRouteState();
+  }
+}
+
+class _NotificationRouteState extends State<MyTestNotificationRoute> {
+  String _msg = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<MyTestNotification>(
+      onNotification: (notification) {
+        setState(() {
+          _msg += "${notification.msg}${notification.count}";
+        });
+      },
+      child: Column(
+        children: <Widget>[
+          Builder(builder: (context) {
+            return RaisedButton(
+              onPressed: () {
+                var notic = MyTestNotification("count:");
+                notic.count++;
+                notic.dispatch(context);
+              },
+              child: Text("check it"),
+            );
+          }),
+          Text(_msg)
+        ],
+      ),
+    );
+  }
+}
