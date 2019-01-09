fdf09ccbf9d65339c5760d1fdb93632f7bb7d998
jingyan.wan <wanjingyan001@yahoo.com>
2019-01-09 23:33:03 +0800

    Activity导包

--- android/app/src/main/java/com/wjy/study1/MainActivity.java
+++ android/app/src/main/java/com/wjy/study1/MainActivity.java
@@ -1,10 +1,5 @@
 package com.wjy.study1;
 
-import android.os.Bundle;
-
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -12,6 +7,10 @@ import android.os.BatteryManager;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "samples.flutter.io/battery";
@@ -21,19 +20,16 @@ public class MainActivity extends FlutterActivity {
         super.onCreate(savedInstanceState);
         GeneratedPluginRegistrant.registerWith(this);
         new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
-                new MethodCallHandler() {
-                    @Override
-                    public void onMethodCall(MethodCall call, Result result) {
-                        if (call.method.equals("getBatteryLevel")) {
-                            int batteryLevel = getBatteryLevel();
-                            if (batteryLevel != -1) {
-                                result.success(batteryLevel);
-                            } else {
-                                result.error("UNAVAILABLE", "Battery level not available.", null);
-                            }
+                (call, result) -> {
+                    if ("getBatteryLevel".equals(call.method)) {
+                        int batteryLevel = getBatteryLevel();
+                        if (batteryLevel != -1) {
+                            result.success(batteryLevel);
                         } else {
-                            result.notImplemented();
+                            result.error("UNAVAILABLE", "Battery level not available.", null);
                         }
+                    } else {
+                        result.notImplemented();
                     }
                 });
     }
